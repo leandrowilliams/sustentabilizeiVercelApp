dc63e448520a211bbe7e4075f5a44a01ac6ed981
leandrowilliams <87885115+leandrowilliams@users.noreply.github.com>
2022-01-19 20:27:24 -0300

    DEPLOY

--- src/paginas/cadastroUsuario/CadastroUsuario.tsx
+++ src/paginas/cadastroUsuario/CadastroUsuario.tsx
@@ -92,7 +92,7 @@ function CadastroUsuario() {
                                     </Button>
                                 </Link>
                                 </Box>
-                                <Box><Button type="submit" variant="contained" className="botao, colorr" >
+                                <Box className="colorr"><Button type="submit" variant="contained" className="botao" >
                                     Cadastrar
                                 </Button>
                                 </Box>
